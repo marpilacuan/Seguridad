c4a0c9779b705935daafaf8cfb8332c081cf3e93
Mar <Mar@172.16.15.120>
2017-05-04 22:54:40 -0500

    Prueba 4

--- src/socketserver/SocketServer.java
+++ src/socketserver/SocketServer.java
@@ -17,6 +17,13 @@ public class SocketServer {
         Servidor server = new Servidor();
         System.out.println("Hola");
         System.out.println("Estableciendo conexion...");
+        System.out.println("Hola");
+        System.out.println("Estableciendo conexion...");
+        System.out.println("Hola");
+        System.out.println("Estableciendo conexion...");
+        System.out.println("Hola");
+        System.out.println("Estableciendo conexion...");
+        
         server.establecerConexion(5555);
         
     }
